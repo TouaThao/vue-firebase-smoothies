655ad7ef01e14dbf7a4d3cd12dde786f7b1af711
Toua Thao <touathao09@gmail.com>
2019-06-03 22:39:43 -0500

    added firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBz2WxDwcHhPA8QCp-rJF58_IPkW9AS32Y",
+    authDomain: "vuefirebasesmoothies.firebaseapp.com",
+    databaseURL: "https://vuefirebasesmoothies.firebaseio.com",
+    projectId: "vuefirebasesmoothies",
+    storageBucket: "vuefirebasesmoothies.appspot.com",
+    messagingSenderId: "558423820165",
+    appId: "1:558423820165:web:1c3ea39c052aeada"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
