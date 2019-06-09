c72099a96a755747a51950a5ba6d51b31a6b0609
Toua Thao <touathao09@gmail.com>
2019-06-08 19:55:56 -0500

    finish delete function

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,25 +1,26 @@
 <template>
-   <div class="add-smoothie container">
-       <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
-       <form @submit.prevent="AddSmoothie">
-           <div class="field title">
-               <label for="title">Smoothie Title</label>
-               <input type="text" name="title" v-model="title" >
-           </div>
-           <div v-for="(ing,index) in ingredients" :key="index">
-               <label for="ingredients">ingredients:</label>
-               <input type="text" name="ingredients" v-model="ingredients[index]">
-           </div>
-           <div class="field add-ingredient">
-               <label for="add-ingredient">Add an ingredient:</label>
-               <input input="text" name="add-ingredient" @keydown.enter.prevent="addIng" v-model="another">
-           </div>
-           <div class="field center align">
-               <p class="red-text" v-if="feedback">{{feedback}}</p>
-               <button class="btn pink">Add Smoothie</button>
-           </div>
-       </form>
-   </div>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="AddSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div v-for="(ing,index) in ingredients" :key="index" class="field">
+        <label for="ingredients">ingredients:</label>
+        <input type="text" name="ingredients" v-model="ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient:</label>
+        <input input="text" name="add-ingredient" @keydown.enter.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center align">
+        <p class="red-text" v-if="feedback">{{feedback}}</p>
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 </<script>
@@ -69,23 +70,38 @@ export default {
             }else{
                 this.feedback = 'You Must Enter A Ingredients'
             }
+        },
+        deleteIng(ing){
+            this.ingredients = this.ingredients.filter(ingredient=>{
+                return ingredient != ing
+            })
         }
     }
 }
 </script>
 
 <style>
-.add-smoothie{
-    margin-top:60px;
-    padding: 20px;
-    max-width: 500px;
+.add-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
 }
 
-.add-smoothie h2{
-    font-size: 2em;
-    margin: 20px auto;
+.add-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
 }
-.add-smoothie .field{
-    margin:20px auto;
+.add-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+ 
+.add-smoothie .delete{
+    cursor:pointer;
+    position:absolute;
+    right: 0;
+    bottom: 16px;
+    color:#aaa;
+    font-size: 1.4em;
 }
 </style>
