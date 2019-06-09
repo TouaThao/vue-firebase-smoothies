19bded477ccf17c2b4d83aab1380b11f10d0e2fc
Toua Thao <touathao09@gmail.com>
2019-06-09 10:40:36 -0500

    finish edit components

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import AddSmoothie from '@/components/AddSmoothie'
+import EditSmoothie from '@/components/EditSmoothie'
 
 Vue.use(Router)
 
@@ -14,9 +15,15 @@ export default new Router({
       component: Index
     },
     {
-      path: '/add- smoothie',
+      path: '/add-smoothie',
       name: 'AddSmoothie',
       component: AddSmoothie
+    },
+    {
+      path: '/edit-smoothie/:smoothie_slug',
+      name: 'EditSmoothie',
+      component: EditSmoothie
     }
+    
   ]
 })
